dca4bcb196fd8da27c23102a662e8c04a6d39edb
shabana <J.maxwell@simplilearn.com>
2023-12-19 06:03:35 -0800

    Add project files.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         int[][] jaggedArray = {
+         new int[] {1, 3, 5},
+         new int[] {2, 4},
+      };
+
+            // print elements of jagged array
+            Console.WriteLine("jaggedArray[1][0]: " + jaggedArray[1][0]);
+            Console.WriteLine("jaggedArray[1][1]: " + jaggedArray[1][1]);
+
+            Console.WriteLine("jaggedArray[0][2]: " + jaggedArray[0][2]);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+        
+    
+
